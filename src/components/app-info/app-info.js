8e7960a03e037b6a369d142d62650668030b69ac
Arsen <arsenminer@gmai.com>
2022-08-09 23:14:39 +0500

    developing wit components

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников  компаний N</h1>
+            <h2>Общее число сотрулников:</h2>
+            <h2>Премию получать:</h2>
+        </div>
+        )
+}
+
+export default AppInfo;
